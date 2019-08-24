111a11b577b2b404ebfd9bc9fa57a36165408c58
shiv pratap singh <pratapshiv1992@gmail.com>
2019-08-25 04:36:15 +0530

    selectedService and activeKey handler implemented

--- src/components/BookAService.jsx
+++ src/components/BookAService.jsx
@@ -6,23 +6,26 @@ import {dripData} from '../resources/dummyData';
 import {days, timeSlots} from '../resources/dummyData';
 
 const BookAService = () => {
+    const [selectedService,setSelectedService] = React.useState({});
+    const [activeKey,setActiveKey] = React.useState("selectService");
+
     return (
         <Container>
             <div>Book a service</div>
-            <Tab.Container id="left-tabs-example" defaultActiveKey="dateTime">
+            <Tab.Container id="left-tabs-example" activeKey={activeKey} defaultActiveKey={activeKey}>
                 <Row>
                     <Col sm={3}>
                         <Nav variant="tabs" className="flex-column">
-                            <Nav.Item><Nav.Link eventKey="selectService">Select Service</Nav.Link></Nav.Item>
-                            <Nav.Item><Nav.Link eventKey="dateTime" disabled={true}>Date/Time</Nav.Link></Nav.Item>
-                            <Nav.Item><Nav.Link eventKey="payment" disabled={true}>Payment</Nav.Link></Nav.Item>
-                            <Nav.Item><Nav.Link eventKey="bookIt" disabled={true}>Book It</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link eventKey="selectService" disabled={activeKey === "selectService" ? false : true}>Select Service</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link eventKey="dateTime" disabled={activeKey === "dateTime" ? false : true} >Date/Time</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link eventKey="payment" disabled={activeKey === "payment" ? false : true}>Payment</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link eventKey="bookIt" disabled={activeKey === "bookIt" ? false : true}>Book It</Nav.Link></Nav.Item>
                         </Nav>
                     </Col>
                     <Col sm={9}>
                         <Tab.Content>
-                            <Tab.Pane eventKey="selectService"><SelectService dripData={dripData}/></Tab.Pane>
-                            <Tab.Pane eventKey="dateTime"><ServiceDateTime days={days} timeSlots={timeSlots} /></Tab.Pane>
+                            <Tab.Pane eventKey="selectService"><SelectService dripData={dripData} setActiveKey={setActiveKey} setSelectedService={setSelectedService} /></Tab.Pane>
+                            <Tab.Pane eventKey="dateTime"><ServiceDateTime selectedService={selectedService} days={days} timeSlots={timeSlots} /></Tab.Pane>
                             <Tab.Pane eventKey="payment">Payment</Tab.Pane>
                             <Tab.Pane eventKey="bookIt">Book It</Tab.Pane>
                         </Tab.Content>
--- src/components/SelectService.jsx
+++ src/components/SelectService.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import {Row, Col, Button} from 'react-bootstrap';
 
-const SelectService = ({dripData}) => {
+const SelectService = ({dripData, setSelectedService, setActiveKey}, i) => {
     const elements = dripData.map(({dripText, dripName}) =>
         <div>
             <Row>
                 <Col sm={8}><h2>{dripName}</h2>
                     <div>{dripText}</div>
                 </Col>
-                <Col sm={3}><Button variant="outline-secondary">Select</Button></Col>
+                <Col sm={3}>
+                    <Button
+                        variant="outline-secondary"
+                        value={33}
+                        onClick={(e) => {
+                            setSelectedService({dripText, dripName});
+                            setActiveKey('dateTime');
+                        }}>
+                        Select
+                    </Button></Col>
             </Row>
             <hr/>
         </div>
