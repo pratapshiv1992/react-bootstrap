ec753a35b1bac5dd31e46f6ef1482fdf98747f09
shiv pratap singh <pratapshiv1992@gmail.com>
2019-08-25 05:27:50 +0530

    arrow icon implemented

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/common.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
