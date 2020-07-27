f77778c2e77e816deacbc73adaed5678d11a9217
Shiv pratap singh <pratapshiv1992@gmail.com>
2020-07-27 12:42:12 +0530

    minor tweak minor tweak

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import BookAService from './components/BookAService';
-import Navbar from './components/Navbar';
-import {Container} from 'react-bootstrap';
+import React from "react";
+import BookAService from "./components/BookAService";
+import Navbar from "./components/Navbar";
+import { Container } from "react-bootstrap";
 
-import './App.css';
-
-const App = (props) => {
-    return <Container style={{maxWidth: "1100px", paddingLeft: "50px", paddingTop: "20px"}}>
-        <Navbar/>
-        <BookAService/>
+import "./App.css";
+const containerStyle = {
+  maxWidth: "1100px",
+  paddingLeft: "50px",
+  paddingTop: "20px"
+};
+const App = props => {
+  return (
+    <Container style={containerStyle}>
+      <Navbar />
+      <BookAService />
     </Container>
-}
+  );
+};
 
 export default App;
